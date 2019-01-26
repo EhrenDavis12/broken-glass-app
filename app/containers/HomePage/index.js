8fc122bb3d5f5424e242a1a365f998e9ae7b8abf
Rex <roydavis99.rd@gmail.com>
2019-01-26 11:50:33 -0700

    asdf

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -17,15 +17,22 @@ import styled from 'styled-components';
 import LOGO from "../../images/BGLogo.png";
 import messages from './messages';
 
-
+const Wrapper = styled.div`
+opacity: 0.7;
+background-color:black;
+padding: 8px;
+height: 100vh;
+`;
+const SolidWrapper = styled.div`
+opacity: 1;
+`;
 
 /* eslint-disable react/prefer-stateless-function */
 export default class HomePage extends React.PureComponent {
   render() {
     return (
       <div className='text-center'>
-        
-        <img src={LOGO} style={{width:600, marginBottom:50, marginTop: 30}}/>
+        <img src={LOGO} style={{ width: 600, marginBottom: 50, marginTop: 30 }} />
         <SearchBar />
       </div>
     );
